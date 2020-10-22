19151f0b62e4f544f2588c6bf314337fbe9da5f7
Yair Daon <yair.daon@gmail.com>
2020-10-22 17:57:01 +0300

    getting closer

--- differ.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#latexdiff --config="PICTUREENV=(?:picture|DIFnomarkup|align|tabular)[\w\d*@]*" paper.tex new_paper.tex > diff.tex
-# latexdiff --exclude-textcmd=”section,subsection,subsubsection” --config=”PICTUREENV=(?:picture|DIFnomarkup|align|tabular)[\w\d*@]*” paper.tex new_paper.tex > diff.tex
-# latexdiff paper.tex new_paper.tex > diff.tex
-#latexmk -pdf -f diff
-latexmk -pdf paper
-latexmk -pdf new_paper
-latexmk -pdf reply
