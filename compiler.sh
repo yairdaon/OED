ce6ba4ac745fa64c7680fe237a8b0d2d760d8893
Yair Daon <yair.daon@gmail.com>
2020-10-26 19:28:35 +0200

    I think I am donegit add reply.tex git add reply.tex

--- compiler.sh
+++ compiler.sh
@@ -2,7 +2,10 @@
 latexdiff --config="PICTUREENV=(?:picture|DIFnomarkup|figure|)[\w\d*@]*" paper.tex new_paper.tex > diff.tex
 #latexdiff --exclude-textcmd=”section,subsection,subsubsection” --config=”PICTUREENV=(?:picture|DIFnomarkup|align|tabular)[\w\d*@]*” paper.tex new_paper.tex > diff.tex
 #latexdiff paper.tex new_paper.tex > diff.tex
-latexmk -pdf diff
 #latexmk -pdf paper
-latexmk -pdf new_paper
 latexmk -pdf reply
+latexmk -pdf reply
+latexmk -pdf new_paper
+latexmk -pdf new_paper
+latexmk -pdf diff
+latexmk -pdf diff
