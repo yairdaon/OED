ff407129800504f66dba4ca92359ab2c17738e5d
Yair Daon <yair.daon@gmail.com>
2021-08-12 13:51:39 +0300

    Test observations pass

--- tests/test_observations.py
+++ tests/test_observations.py
@@ -8,6 +8,17 @@ from observations import PointObservation, DiagObservation
 
 COLORS = ['r', 'g', 'b', 'k', 'c', 'm', 'y']
 
+
+def align_eigenvectors(P):
+    P = np.einsum('ij, i -> ij', P, np.exp(-1j * np.angle(P[:, 0])))
+    assert np.all(P[:, 0].real > 0)
+    assert np.all(np.abs(P[:, 0].imag) < 1e-12)
+    ind = np.argsort(P[:, 1])
+    P = P[ind, :]
+    # P = np.sort(P, axis=0)
+    return P
+
+
 def test_point_observation():
     k = 7
     obs = PointObservation(N=1400, L=2, meas=np.linspace(0.1, 1.9, 50, endpoint=False))
@@ -23,9 +34,9 @@ def test_point_observation():
         plt.show()
 
 
-@pytest.mark.parametrize("transform", ['dct', 'fft'])
+@pytest.mark.parametrize("transform", ['fft', 'dct'])
 def test_diagonal_observation_eigenvectors(transform):
-
+    np.random.seed(342424)
     n = 3  # len(COLORS)
     singular_values = np.random.randn(n) ** 2
     # singular_values[1] = 0
@@ -33,19 +44,14 @@ def test_diagonal_observation_eigenvectors(transform):
                           N=200,
                           random_U=True,
                           transform=transform)
-    k = obs.N // 5
 
     D, P = np.linalg.eig(obs.OstarO)
     xtra = P[:, n]
     P = P[:, :n].T.conjugate()
-    P = np.einsum('ij, i -> ij', P, np.sign(P[:, k].real))
-    assert np.all(P[:, k].real > 0)
-    P = P[np.argsort(P[:, k].real)]
+    P = align_eigenvectors(P)
 
     eigs = np.vstack([obs.eigenvector(i) for i in range(n)])
-    eigs = np.einsum('ij, i -> ij', eigs, np.sign(eigs[:, k].real))
-    assert np.all(eigs[:, k].real > 0)
-    eigs = eigs[np.argsort(eigs[:, k].real)]
+    eigs = align_eigenvectors(eigs)
 
     assert_allclose(np.linalg.norm(eigs, axis=1), 1)
     assert_allclose(np.linalg.norm(P, axis=1), 1)
@@ -63,14 +69,14 @@ def test_diagonal_observation_eigenvectors(transform):
             eig = eigs[i, :]
             real_diff = np.max(np.abs(p.real - eig.real))
             imag_diff = np.max(np.abs(p.imag - eig.imag))
-            real_diff = f'{i} {real_diff:.4f}'
-            imag_diff = f'{i} {imag_diff:.4f}'
+            real_diff = f'{i} Error = {real_diff:.4f}'
+            imag_diff = f'{i} Error = {imag_diff:.4f}'
             ax[0, 0].plot(obs.x, p.real, color='r', label=i)
             ax[0, 1].plot(obs.x, p.imag, color='r', label=i)
             ax[1, 0].plot(obs.x, eig.real, color=color, linestyle='-', label=real_diff)
-            ax[1, 0].plot(obs.x, 0.005 + p.real, color=color, linestyle=':')
+            ax[1, 0].plot(obs.x, p.real, color=color, linestyle=':')
             ax[1, 1].plot(obs.x, eig.imag, color=color, linestyle='-', label=imag_diff)
-            ax[1, 1].plot(obs.x, 0.005 + p.imag, color=color, linestyle=':')
+            ax[1, 1].plot(obs.x, p.imag, color=color, linestyle=':')
         ax[0, 0].plot(obs.x, xtra, label='$n+1$', color='k')
         ax[0, 1].plot(obs.x, xtra, label='$n+1$', color='k')
         ax[1, 0].legend()
@@ -79,8 +85,8 @@ def test_diagonal_observation_eigenvectors(transform):
         # ax[0].set_title(f"First $n={n}$ eigs of a diagonalizable " + OstarO)
         ax[0, 0].set_title(f"First $n+1={n + 1}$ eigs of " + OstarO + '  (real)')
         ax[0, 1].set_title(f"First $n+1={n + 1}$ eigs of " + OstarO + '  (imaginary)')
-        ax[1, 0].set_title(f"First $n={n}$ modes of {obs.transform}")
-        ax[1, 1].set_title(f"First $n={n}$ modes of {obs.transform}")
+        ax[1, 0].set_title(f"First $n={n}$ modes of {obs.transform} (real)")
+        ax[1, 1].set_title(f"First $n={n}$ modes of {obs.transform} (imaginary)")
 
         # ind = np.where(np.abs(D) > 1e-9)[0]
         # ax[3].plot(np.arange(n), D[:n], label="e_i") # "'$\mathbf{e}_i$')
