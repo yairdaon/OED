More success at solving the inverse problem. Posterior mean is correct. Posterior STD is also correct (t least up to a scaling factor. to_time and to_freq are operating in function space. Consequently, they are not self adjoint in the standard linear algebraic sense.(test_probability.py::test_posterior)

src/multiplier.py
@@ -29,17 +29,30 @@ class FourierMultiplier(Operator):
         self.multiplier = None # Need to implement this on particular case
         self.transform = transform
         if self.transform == 'dct':
-            self.freqs = fft.fftfreq(self.N, d=self.h)
-            self.to_freq_domain = partial(fft.dct, norm='ortho', type=2)
-            self.to_time_domain = partial(fft.dct, norm='ortho', type=3)
+            # self.to_freq_domain = partial(fft.dct, norm='ortho', type=2)
+            # self.to_time_domain = partial(fft.dct, norm='ortho', type=3)
             self.freqs = np.arange(self.N) / self.L
         # elif self.transform == 'dst':
         #     self.to_freq_domain = partial(fft.dst, norm='ortho')
         #     self.to_time_domain = partial(fft.idst, norm='ortho')
         elif self.transform == 'fft':
             self.freqs = fft.fftfreq(self.N, d=self.h)
-            self.to_freq_domain = partial(fft.fft, norm='ortho')
-            self.to_time_domain = partial(fft.ifft, norm='ortho')
+            # self.to_freq_domain = partial(fft.fft, norm='ortho')
+            # self.to_time_domain = partial(fft.ifft, norm='ortho')
+
+    def to_freq_domain(self, x, axis=-1):
+        if self.transform == 'dct':
+            return fft.dct(x, norm='ortho', type=2, axis=axis) * self.sqrt_h
+        elif self.transform == 'fft':
+            return fft.fft(x, norm='ortho', axis=axis) * self.sqrt_h
+        # elif self.transform == 'dst':
+        #     return fft.dst(...)
+
+    def to_time_domain(self, x, axis=-1):
+        if self.transform == 'dct':
+            return fft.dct(x, norm='ortho', type=3, axis=axis) / self.sqrt_h
+        elif self.transform == 'fft':
+            return fft.ifft(x, norm='ortho', axis=axis) / self.sqrt_h
 
     def eigenfunction(self, i):
         if self.transform == 'fft':
@@ -50,11 +63,10 @@ class FourierMultiplier(Operator):
         return lambda x: eigen(x) / norm
 
     def eigenvector(self, i):
-        v = self.eigenfunction(i)(self.x)
-        return v / np.linalg.norm(v)
+        return self.eigenfunction(i)(self.x)
 
-    def coeff2u(self, coeff):
-        return self.to_time_domain(coeff) / self.sqrt_h
+    # def coeff2u(self, coeff):
+    #     return self.to_time_domain(coeff) / self.sqrt_h
 
     def normal(self, n_sample=1):
         if self.transform == 'fft':
@@ -65,14 +77,14 @@ class FourierMultiplier(Operator):
             Z = randn(n_sample, self.N)
         return Z
 
-    def mult2time(self, mult):
-        """mult is assumed 2D!!!"""
-        assert len(mult.shape) == 2
-        # M4 = ifft(mult, axis=0)
-        # M4 = ifft(M4.H, axis=0)
-        M = self.to_time_domain(mult, axis=0)
-        M = self.to_time_domain(M.conjugate().T, axis=0)
-        return M
+    # def mult2time(self, mult):
+    #     """mult is assumed 2D!!!"""
+    #     assert len(mult.shape) == 2
+    #     # M4 = ifft(mult, axis=0)
+    #     # M4 = ifft(M4.H, axis=0)
+    #     M = self.to_time_domain(mult, axis=0)
+    #     M = self.to_time_domain(M.conjugate().T, axis=0)
+    #     return M
 
     def _matvec(self, v):
         shp = v.shape

src/observations.py
@@ -23,10 +23,10 @@ class PointObservation(Observation):
 
         if self.transform == 'fft':
             for k in range(self.N):
-                self.multiplier[:, k] = self.eigenfunction(k)(self.meas) / np.sqrt(self.N)
+                self.multiplier[:, k] = self.eigenfunction(k)(self.meas)
         elif self.transform in ('dct', 'dst'):
             for k in range(self.N):
-                self.multiplier[:, k] = self.eigenfunction(k)(self.meas) / np.sqrt(0.5 * self.N)
+                self.multiplier[:, k] = self.eigenfunction(k)(self.meas)
 
     def __str__(self):
         return 'Point observations ' + ', '.join([f'{me:.4f}' for me in self.meas])

src/probability.py
@@ -27,14 +27,15 @@ class Prior(FourierMultiplier):
         # self.ind = ind
 
     def sample(self, return_coeffs=False, n_sample=1):
-        """ Generate a sample and return its coefficients if needed"""
+        """ Generate a sample and return its coefficients if needed. This effectively uses the
+        Karhunen-Loeve expansion"""
         assert n_sample > 0
         coeffs = self.normal(n_sample)
 
         coeffs = np.einsum('ij, j->ij', coeffs, np.power(self.multiplier, 0.5))
         # coeffs[:, 0] = 0
 
-        u0 = self.coeff2u(coeffs)
+        u0 = self.to_time_domain(coeffs)
         if return_coeffs:
             return u0, coeffs
         return u0
@@ -71,7 +72,9 @@ class Posterior(FourierMultiplier):
         self.AstarA = np.einsum('ij, jk-> ik', self.A_H, self.A)
         self.precision = self.AstarA / self.sigSqr + np.diag(self.prior.inv_mult)
         Sigma = np.linalg.inv(self.precision)
+        Sigma = self.to_time_domain(self.to_time_domain(Sigma).conjugate().T).conjugate().T * self.h
 
+        # Because Sigma = (to_freq* to_time(multiplier)*)*
         #         self.A = A
         #         self.Sigma = Sigma
         #         self.precision = precision
@@ -84,7 +87,7 @@ class Posterior(FourierMultiplier):
         # mean[80:] = 0
 
         self.m = self.to_time_domain(mean)
-        self.ptwise = np.sqrt(np.abs(np.diag(self.mult2time(Sigma)))).real
+        self.ptwise = np.sqrt(np.abs(np.diag(Sigma))).real
 
     def utility(self, meas):
         obs = PointObservation(meas=meas, N=self.N, L=self.L)

tests/test_forward.py
@@ -31,7 +31,7 @@ def test_heat(transform):
 
     # Analytic
     coeffsT = coeffs0 * fwd.multiplier
-    uT = prior.coeff2u(coeffsT).squeeze()
+    uT = prior.to_time_domain(coeffsT).squeeze()
     # uT = np.squeeze(uT)
     # Numeric solution
     uT_numeric = fwd(u0)
@@ -42,8 +42,9 @@ def test_heat(transform):
     interpolant = interp1d(fwd.x, uT)
     measure_success = np.allclose(obs(uT), interpolant(obs.meas), atol=1e-2, rtol=0)
 
-    if not (numeric_success and inversion_success and measure_success):
-        assert True
+    success = numeric_success and inversion_success and measure_success
+    if success:
+        assert success
     else:
         plt.plot(fwd.x, u0.real, label='IC')
         plt.plot(fwd.x, uT.real, label='Analytic FC')
@@ -54,6 +55,8 @@ def test_heat(transform):
         # plt.plot(prior.x, prior.inverse(uT), label= prior.inv_str + 'FC')
         plt.scatter(obs.meas, obs(uT).real, label='Measurements of FC', marker='*', s=10, color='w', zorder=10)
         plt.legend()
+        title = f'{transform} successes: numeric {numeric_success} inversion {inversion_success} measure {measure_success}'
+        plt.title(title)
         plt.show()
-        assert False
+        assert success
 

tests/test_multiplier.py
@@ -24,20 +24,6 @@ def multiplier(transform):
 
 
 @pytest.mark.parametrize('transform', ['dct', 'fft'])
-def test_factor(multiplier):
-    """Test the factor defined in Operator class"""
-    for k in np.where(multiplier.multiplier)[0]:
-        func = multiplier.eigenfunction(k)(multiplier.x)
-        assert abs(multiplier.norm(func) - 1) < 1e-9
-
-        vec = multiplier.eigenvector(k)
-        assert abs(np.linalg.norm(vec) - 1) < 1e-9
-
-        factor = 1 / np.linalg.norm(func)
-        assert abs(factor - multiplier.multiply_vec2make_la_norm_equal_func_norm) < 1e-9
-
-
-@pytest.mark.parametrize('transform', ['dct', 'fft'])
 def test_matvec(multiplier):
     """Test that if we input a constant vector, the output is also constant (???)"""
     v = np.ones(multiplier.N)
@@ -65,10 +51,13 @@ def test_matrix_hermitian(multiplier):
 @pytest.mark.parametrize('transform', ['dct', 'fft'])
 def test_linearoperator(multiplier):
     """Test a multiplier functions as a proper LinearOperator object. We check
-     we can solve a linear system with GMRES and conjugate gradients.
-     Note that we create b to be in the range of the multiplier,
+    we can solve a linear system with GMRES and conjugate gradients.
+    Note that we create b to be in the range of the multiplier,
     otherwise this would not work."""
     b = sum([np.random.randn() * multiplier.eigenvector(i) for i in np.where(multiplier.multiplier)[0]])
+    M = multiplier.matrix
+    err = np.abs(M - M.conjugate().T).max()
+    assert err < 1e-8
     x, _ = cg(multiplier, b)
     b_hat = multiplier(x)
     assert_allclose(b, b_hat)
@@ -78,17 +67,19 @@ def test_linearoperator(multiplier):
     assert_allclose(b, b_hat)
 
 
+@pytest.mark.xfail(reason="Need to consider a proper inner product")
 @pytest.mark.parametrize('transform', ['dct', 'fft'])
 def test_orthogonality(multiplier):
-    """We check the matrices of to_freq_domain and to_time_domain are orthogonal / unitary.
+    """We check the matrices of to_freq_domain and to_time_domain are orthogonal / unitary up to a constant.
     We also check they are each others' inverse.
     """
+    h = multiplier.h
     identity = np.eye(multiplier.N)
     forward = multiplier.to_freq_domain(identity, axis=0)
-    assert_allclose(np.dot(forward, forward.conjugate().T), identity, atol=1e-9, rtol=0)
+    assert_allclose(np.dot(forward, forward.conjugate().T) / h , identity, atol=1e-9, rtol=0)
 
     inverse = multiplier.to_time_domain(identity, axis=0)
-    assert_allclose(np.dot(inverse, inverse.conjugate().T), identity, atol=1e-9, rtol=0)
+    assert_allclose(np.dot(inverse, inverse.conjugate().T) * h, identity, atol=1e-9, rtol=0)
 
     assert_allclose(np.dot(inverse, forward), identity, atol=1e-9, rtol=0)
     assert_allclose(np.dot(forward, inverse), identity, atol=1e-9, rtol=0)
@@ -102,13 +93,13 @@ def test_orthogonality(multiplier):
 def test_eigenvectors_agree(multiplier):
     """We check that the eigenvectors we get from transforming the standard basis
     vectors are the same we get from the multiplier object iteslf."""
-    for i in range(multiplier.N//2, multiplier.N):
-        eigenvector = np.zeros(multiplier.N)
-        eigenvector[i] = 1
-        eigenvector = multiplier.to_time_domain(eigenvector)
-        class_eigenvector = multiplier.eigenvector(i)
-        diff = np.abs(eigenvector - class_eigenvector)
-        assert_allclose(diff, 0, atol=1e-13, rtol=0)
+    for i in range(multiplier.N):
+        eigenfunction = np.zeros(multiplier.N)
+        eigenfunction[i] = 1
+        eigenfunction = multiplier.to_time_domain(eigenfunction)
+        class_eigenfunction = multiplier.eigenfunction(i)(multiplier.x)
+        diff = np.abs(eigenfunction - class_eigenfunction)
+        assert_allclose(diff, 0, atol=1e-11, rtol=0)
 
 
 @pytest.mark.parametrize('transform', ['fft', 'dct'])
@@ -117,7 +108,6 @@ def test_basis_matrix_agree(multiplier):
     forward = multiplier.to_time_domain(identity, axis=0)
     U = np.vstack(multiplier.eigenvector(i) for i in range(multiplier.N)).T
     assert_allclose(U, forward, atol=1e-9, rtol=0)
-    assert_allclose(np.dot(U.conjugate().T, forward), identity, atol=1e-9, rtol=0)
 
 
 @pytest.mark.parametrize("transform", ['dct', 'fft'])
@@ -125,9 +115,9 @@ def test_eigen_norm(multiplier):
     """ Verify norm of eigenfunctions is 1 in the L2 sense and norm
     of eigenvector is 1 in the standaed linear algebraic sense"""
     for i in range(multiplier.N):
-        eigenvector = multiplier.eigenvector(i)
-        assert not np.any(np.isnan(eigenvector))
-        assert abs(np.linalg.norm(eigenvector) - 1) < 1e-9
+        # eigenvector = multiplier.eigenvector(i)
+        # assert not np.any(np.isnan(eigenvector))
+        # assert abs(np.linalg.norm(eigenvector) - 1) < 1e-9
 
         eigenfunction = multiplier.eigenfunction(i)(multiplier.x)
         assert abs(multiplier.norm(eigenfunction) - 1) < 1e-9
@@ -215,15 +205,15 @@ def test_eigenvector_and_eigenfunction_agree_to_normalization(transform):
     for i in range(multiplier.N):
         ef = multiplier.eigenfunction(i)(multiplier.x)
         ev = multiplier.eigenvector(i)
-        assert_allclose(ev, ef/np.linalg.norm(ef), rtol=0, atol=1e-9)
+        assert_allclose(ev, ef, rtol=0, atol=1e-9)
 
 
-@pytest.mark.parametrize("transform", ['dct', 'fft'])
-def test_coeff2u(transform):
-    """Test the function that transforms a coefficient vector to a ***function***.
-    We verify a sample and its coefficients are the same when we compare via coeff2u."""
-    prior = Prior(L=3, N=30, transform=transform, gamma=-0.6)
-    samples, coefficients = prior.sample(return_coeffs=True, n_sample=3)
-    for sample, coefficient in zip(samples, coefficients):
-        calculated_sample = prior.coeff2u(coefficient)
-        assert_allclose(sample, calculated_sample, rtol=0, atol=1e-12)
+# @pytest.mark.parametrize("transform", ['dct', 'fft'])
+# def test_coeff2u(transform):
+#     """Test the function that transforms a coefficient vector to a ***function***.
+#     We verify a sample and its coefficients are the same when we compare via coeff2u."""
+#     prior = Prior(L=3, N=30, transform=transform, gamma=-0.6)
+#     samples, coefficients = prior.sample(return_coeffs=True, n_sample=3)
+#     for sample, coefficient in zip(samples, coefficients):
+#         calculated_sample = prior.coeff2u(coefficient)
+#         assert_allclose(sample, calculated_sample, rtol=0, atol=1e-12)

tests/test_observations.py
@@ -7,23 +7,29 @@ from scipy.interpolate import interp1d
 
 from tests.helpers import align_eigenvectors
 
+from src.probability import Prior
 COLORS = ['r', 'g', 'b', 'k', 'c', 'm', 'y']
 
 
-def test_point_observation():
+@pytest.mark.parametrize("transform", ['fft', 'dct'])
+def test_point_observation(transform):
     """Test we can measure an observable correctly."""
-    k = 7
-    obs = PointObservation(N=1400, L=2, meas=np.linspace(0.1, 1.9, 50, endpoint=False))
-    u = obs.eigenvector(k)
-    measured = interp1d(obs.x, u)(obs.meas)
-
-    if np.allclose(measured, obs(u), rtol=0, atol=1e-3):
-        assert True
+    N, L = 2000, 3
+    prior = Prior(N=N, L=L, transform=transform, gamma=-1.6)
+    u = prior.sample(return_coeffs=False).squeeze()
+    measurements = np.linspace(0, L, 33, endpoint=False)
+    obs = PointObservation(N=N, L=L, meas=measurements)
+    measured = interp1d(prior.x, u)(obs.meas)
+    err = np.abs(measured - obs(u)).max()
+    if err < 1e-3:
+        assert err < 1e-3
     else:
         plt.figure(figsize=(6, 3))
         plt.plot(obs.x, u)
-        plt.scatter(obs.meas, obs(u).real)
+        plt.scatter(obs.meas, obs(u).real, color='r')
+        plt.title(f'Max abs err {err}')
         plt.show()
+        assert err < 1e-3
 
 
 @pytest.mark.parametrize("transform", ['fft', 'dct'])

tests/test_probability.py
@@ -17,7 +17,7 @@ def prior(transform):
     np.random.seed(253234236)
     N = 500
     L = 3
-    gamma = -.6
+    gamma = -1.8
     return Prior(gamma=gamma, N=N, L=L, transform=transform)
 
 @pytest.mark.parametrize("transform", ['dct', 'fft'])
@@ -25,29 +25,45 @@ def test_prior_sample_from_coefficients(prior):
 
     sample, coefficients = prior.sample(return_coeffs=True)
     sample = sample.squeeze()
-    sample_from_cefficients = prior.coeff2u(coefficients.squeeze())
+    sample_from_cefficients = prior.to_time_domain(coefficients.squeeze())
     assert_allclose(sample, sample_from_cefficients)
-
 
 
 @pytest.mark.parametrize("transform", ['dct', 'fft'])
 def test_prior_sample_mean(prior):
     """Test that sampling the prior we get zero mean."""
-    n_sample = 500000
+    n_sample = 5000
+    k = 10
+    get_mean = lambda : prior.sample(return_coeffs=False, n_sample=n_sample).mean(axis=0)
+    sample = sum(Parallel(n_jobs=6)(delayed(get_mean)() for _ in range(k))) / k
+    assert sample.shape == (prior.N,)
 
-    sample, coeff = prior.sample(return_coeffs=True, n_sample=n_sample)
-    assert sample.shape == (n_sample, prior.N)
-    assert coeff.shape == (n_sample, prior.N)
-
-    mean = np.mean(sample, axis=0)
-    assert mean.shape == (prior.N,)
-    assert_allclose(mean, np.zeros_like(mean), rtol=0, atol=1e-2)
+    assert_allclose(sample, np.zeros_like(sample), rtol=0, atol=1e-2)
 
 
 @pytest.mark.parametrize("transform", ['dct', 'fft'])
 def test_prior_sample_covariance(prior):
     """Test the empirical covariance is the same as the matrix representation of the prior.
-    TODOs - check eigenvectors and eigenvalues agree???"""
+    TODOs - check eigenvectors and eigenvalues agree???
+
+    We compare the empiric covariance with the operator. The covariance operator is defined (see Wikipedia) as
+
+     C: H -> H , <Cx, y> = \int_H <x,z><y,z> dP(z).
+
+    We think of H as as discretization of L2. H contains functions that
+    are constant on every interval [nh, nh+h) and the inner product is
+
+    <x,y> = \int_0^L x(u)y*(u) du = h sum_i=1^N x_iy_i*.
+
+    Thus (\dot is the linear algebraic dot product):
+
+    h sum (Cx)_iy_i* = <Cx, y> = h^2 \int_H x\dotz y \dot z dP(z).
+
+    Taking x = e_i and y = e_j, we get that
+
+    C_ij = h E[z_i z_j].
+
+    On the left - the covariance operator. On the right - the empiric covariance."""
     n_sample = 50000
     k = 40
     function = lambda: np.cov(prior.sample(n_sample=n_sample), rowvar=False)
@@ -56,35 +72,24 @@ def test_prior_sample_covariance(prior):
     assert empiric_covariance.shape == (prior.N, prior.N)
     assert_allclose(empiric_covariance, empiric_covariance.conjugate().T)
 
-    operator_covariance = prior.matrix
-    empiric_ratio = np.mean(operator_covariance / empiric_covariance)
-    assert abs(empiric_ratio - prior.h) < 1e-4
-    assert_allclose(operator_covariance, empiric_covariance * prior.h, rtol=0, atol=1e-4)
-    # operator_covariance ~ empiric_covariance * h
+    # See details in the docs above.
+    assert_allclose(prior.matrix, empiric_covariance * prior.h, rtol=0, atol=1e-4)
 
-@pytest.mark.parametrize("transform", ['dct', 'fft'])
-def test_prior_covariance_multiplier(prior):
-    operator_covariance = prior.matrix
-    covariance = prior.mult2time(np.diag(prior.multiplier))
-    assert covariance.shape == (prior.N, prior.N)
-    assert_allclose(covariance, operator_covariance, rtol=0, atol=1e-9)
 
 @pytest.mark.parametrize("transform", ['dct', 'fft'])
 def test_posterior(prior):
 
-    sig = 0.0000005
+    sig = 0.000001
     time = 5e-2
     alpha = 0.6
     L, N, transform = prior.L, prior.N, prior.transform
 
-    meas = np.linspace(0.05, L - 0.05, endpoint=False, num=7)
-    meas += np.random.normal(scale=0.01, size=meas.size)
-    meas = np.random.uniform(low=0.05, high=L-0.05, size=79)
+    meas = np.random.uniform(low=0, high=L, size=500)
 
     np.random.seed(134567)
     obs = PointObservation(meas=meas, L=L, N=N, transform=transform)
     fwd = Heat(N=N, L=L, alpha=alpha, time=time, transform=transform)
-    prior.multiplier[4:] = 0
+    # prior.multiplier[4:] = 0
     post = Posterior(fwd=fwd,
                      prior=prior,
                      sigSqr=sig**2,
@@ -94,10 +99,10 @@ def test_posterior(prior):
     u0 = prior.sample(return_coeffs=False).squeeze()
 
     uT = fwd(u0)
-    data = obs(uT) # + np.random.normal(scale=sig, size=obs.shape[0])
+    data = obs(uT) + np.random.normal(scale=sig, size=obs.shape[0])
     post.update(obs, data)
 
-    fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(16, 6))
+    fig, ax = plt.subplots(nrows=2, ncols=1, figsize=(6, 16))
     ax[0].plot(fwd.x, u0.real, label='IC')
     ax[0].plot(fwd.x, uT.real, label='FC')
     ax[0].scatter(obs.meas, np.dot(post.A, post.to_freq_domain(u0)).real, label='Matrix FC')
@@ -116,4 +121,5 @@ def test_posterior(prior):
     # tra = post.to_freq_domain(post.m)
     # plt.close()
     # plt.plot(tra)
+    plt.tight_layout()
     plt.show()