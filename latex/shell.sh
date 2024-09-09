60a3c6a32473f09268353b3d688cec4e2d4c67dd
Yair Daon <yair.daon@gmail.com>
2024-09-09 15:51:54 +0300

    baa

--- latex/shell.sh
+++ latex/shell.sh
@@ -1,10 +1,6 @@
+latexmk -c
 latexpand ba.tex > flat.tex
-latexdiff --math-markup=OFF old.tex flat.tex > diff.tex
-latexmk --pdf ba.tex
-latexmk --pdf ba.tex
-latexmk --pdf ba.tex
+latexdiff --math-markup=OFF old/old_flat.tex flat.tex > diff.tex
 latexmk --pdf ba.tex
 latexmk --pdf diff.tex
-latexmk --pdf diff.tex
-latexmk --pdf diff.tex
-latexmk --pdf diff.tex
+
